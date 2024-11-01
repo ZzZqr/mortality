9c9fc53382e9162c13fb8ebe16879564d7996274
zhengqirui <201250229@smail.nju.edu.cn>
2024-11-01 15:49:03 +0800

    change

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import joblib
+import numpy as np
+import pandas as pd
 # 定义模型文件的完整路径
 model_path = 'model/xgb_model.joblib'
 
@@ -13,17 +15,14 @@ st.write('Enter some input features to make predictions:')
 
 # 创建输入框用于用户输入特征
 new_onset_shock = st.selectbox('new_onset_shock', options=["Yes", "No"])
-DBIL = st.number_input('DBIL (μmol/L)', min_value=0.00, max_value=50.00, step=0.01)
+ALT = st.number_input('ALT (U/L)', min_value=5.0, max_value=800.0, step=0.1)
 SOFA = st.number_input('SOFA', min_value=0, max_value=9, step=1)
 MDR = st.selectbox('MDR', options=["Yes", "No"])
-los_of_ICU = st.number_input('Los_of_ICU (day)', min_value=1, max_value=150, step=1)
-TBIL = st.number_input('TBIL (μmol/L)', min_value=1.0, max_value=100.0, step=0.1)
-SCr = st.number_input('SCr (μmol/L)', min_value=0.0, max_value=300.0, step=0.1)
 pre_shock = st.selectbox('pre_shock', options=["Yes", "No"])
 area_of_burn = st.number_input('area_of_burn (%)', min_value=20, max_value=100, step=1)
 three = st.number_input('Ⅲ (%)', min_value=0, max_value=80, step=1)
-inhalation_damage = st.selectbox('inhalation_damage', options=["Yes", "No"])
-ALB = st.number_input('ALB (g/L)', min_value=5.0, max_value=50.0, step=0.1)
+sepsis = st.selectbox('sepsis', options=["Yes", "No"])
+type_of_burn = st.selectbox('type_of_burn ', min_value=1, max_value=5, step=1)
 
 if new_onset_shock == "Yes":
     new_onset_shock = 1
@@ -40,22 +39,22 @@ if pre_shock == "Yes":
 else:
     pre_shock = 0
 
-if inhalation_damage == "Yes":
-    inhalation_damage = 1
+if sepsis == "Yes":
+    sepsis = 1
 else:
-    inhalation_damage = 0
+    sepsis = 0
 
 # 定义应用程序行为
 if st.button('Predict'):
     # 将用户输入的特征转换为模型所需的输入格式
-    input_features = [[SOFA, new_onset_shock, MDR, los_of_ICU, TBIL, SCr, pre_shock, area_of_burn, three, inhalation_damage, ALB]]
-
+    input_features = np.array([[sepsis, type_of_burn, area_of_burn, three, pre_shock, new_onset_shock, MDR, ALT, SOFA]])
+    df = pd.DataFrame(input_features, columns=["sepsis", "type_of_burn", "area_of_burn", "III", "pre_shock", "new_onset_shock", "MDR", "ALT", "SOFA"])
     # 使用加载的模型进行预测
-    prediction = loaded_model.predict_proba(input_features)
+    prediction = loaded_model.predict_proba(df)
     print(prediction)
 
     # 显示预测结果
-    st.write(f'Sepsis Prediction:{prediction[0][0]}')
+    st.write(f'Mortality Probability Prediction:{prediction[0][0]}')
 # # 运行Streamlit应用程序
 # if __name__ == '__main__':
 #     st.run()
